0eee76a0e68989ed0224859dd5e561965289af6b
iiou16 <iiou16@gmail.com>
2019-05-07 01:30:01 +0900

    Lecture18 Goalしたときに、Playerの物理演算をoffにしてユニティちゃんにポーズを取らせる

---
+++ Assets/goalChecker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goalChecker : MonoBehaviour
+{
+    public GameObject unityChan;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Goalしたときの処理
+    private void OnTriggerEnter(Collider other)
+    {
+        // 物理演算をoff
+        other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+
+        // Unityちゃんへの処理
+        unityChan.transform.LookAt(Camera.main.transform);
+        unityChan.GetComponent<Animator>().SetTrigger("Goal");
+    }
+}
