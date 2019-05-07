1308dbf48a0602b8ac7fbd5789af9ddb104b5403
iiou16 <iiou16@gmail.com>
2019-05-08 02:50:31 +0900

    Lecture19 ゴールしたときにGBMを切り替えてRetryボタンを表示する

--- Assets/goalChecker.cs
+++ Assets/goalChecker.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class goalChecker : MonoBehaviour
 {
     public GameObject unityChan;
+    public AudioSource gameBGM;
+    public AudioSource goalBGM;
+    public GameObject retryButton;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        retryButton.SetActive(false);
     }
 
     // Update is called once per frame
@@ -27,5 +31,17 @@ public class goalChecker : MonoBehaviour
         // Unityちゃんへの処理
         unityChan.transform.LookAt(Camera.main.transform);
         unityChan.GetComponent<Animator>().SetTrigger("Goal");
+
+        // BGMの切り替え
+        gameBGM.Stop();
+        goalBGM.Play();
+
+        // Goal時にretryボタンを表示
+        retryButton.SetActive(true);
+    }
+
+    public void RetryStage()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
